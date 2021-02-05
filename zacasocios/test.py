346b12ba16b5ee1a14e3c63acf5917631edaed01
Sergio Txiki <sergio@infovit.net>
2021-02-05 20:50:37 +0100

    .

--- zacasocios/test.py
+++ zacasocios/test.py
@@ -1,19 +1,51 @@
+#!/usr/bin/python
+
 import re
 import json
 import requests
-
-url = 'https://zacatrus.es/'
-apiuser = 'odoo'
-apipass = 'p9U4Lap0vF'
-
 import string
 import random
+import sys
+import logging
+import hmac, base64, struct, hashlib, time, os
 
+_logger = logging.getLogger(__name__)
 
+url = 'https://zacatrus.es/rest/all/V1/'
+apiuser = 'zaca24'
+apipass = 'carracacosa1'
 
 def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
 	return ''.join(random.choice(chars) for _ in range(size))
 
+def normalize(key):
+	"""Normalizes secret by removing spaces and padding with = to a multiple of 8"""
+	k2 = key.strip().replace(' ','')
+	# k2 = k2.upper()	# skipped b/c b32decode has a foldcase argument
+	if len(k2)%8 != 0:
+		k2 += '='*(8-len(k2)%8)
+	return k2
+
+def get_hotp_token(secret, intervals_no):
+	"""This is where the magic happens."""
+	key = base64.b32decode(normalize(secret), True) # True is to fold lower into uppercase
+	msg = struct.pack(">Q", intervals_no)
+	h = bytearray(hmac.new(key, msg, hashlib.sha1).digest())
+	o = h[19] & 15
+	h = str((struct.unpack(">I", h[o:o+4])[0] & 0x7fffffff) % 1000000)
+	return prefix0(h)
+
+def get_totp_token():
+	secret = 'KPX24GQ2O553RI3OIBHEVIKQUYNH4YJ523WNFYL72AHNLUBNL5X7JJQDUXIOYIDBN3PHKAWZAFYNM6QNIPOION6BZHCK3Q3EWUCESKBS22WN5NLLDY2YCRUGTPZ56Q3WJUUQLPUNY2VEZQSU6ASOZDAF4EMBOBCUWZEXHXGCUEW3I5ZXV2WHY46GJAOFBZU7XD2C756M2KBHM'
+
+	"""The TOTP token is just a HOTP token seeded with every 30 seconds."""
+	return get_hotp_token(secret, intervals_no=int(time.time())//30)
+
+def prefix0(h):
+	"""Prefixes code with leading zeros if missing."""
+	if len(h) < 6:
+		h = '0'*(6-len(h)) + h
+	return h
 
 class Fichas():
 	def __init__(self, url, username, password):
@@ -22,17 +54,24 @@ class Fichas():
 		self.url = url
 
 	def _getToken(self):
-		data = {"username":self.username,"password": self.password}
+		data = {"username":self.username,"password": self.password, 'otp': get_totp_token()}
+		print(data)
 
-		response = requests.post(self.url + 'integration/admin/token', json=data, headers={})
+		response = requests.post(self.url + 'tfa/provider/google/authenticate', json=data, headers={})
+		print (response)
 		return response.json()
 
-	def _getData(self, url):
-		hed = {'Authorization': 'Bearer ' + self._getToken()}
-
-		#url = 'https://api.xy.com'
-		response = requests.get(self.url + url, json={}, headers=hed)
-		return response.json()
+	def _getData(self, url, postParams=False):
+		token = self._getToken()
+		if token:
+			hed = {'Authorization': 'Bearer ' + token}
+			if postParams:
+				response = requests.post(self.url + url, headers=hed, json=postParams)
+			else:
+				response = requests.get(self.url + url, headers=hed)
+			return response.json()
+		else:
+			return False
 
 	def _postData(self, url, data):
 		hed = {'Authorization': 'Bearer ' + self._getToken()}
@@ -40,6 +79,56 @@ class Fichas():
 		#url = 'https://api.xy.com'
 		response = requests.post(self.url + url, json=data, headers=hed)
 		return response.json()
+
+
+
+	# New methods (Amasty reward extension)
+	def getCustomerByEmail(self, email):
+		sCriteria = "searchCriteria[filterGroups][0][filters][0][field]=email"
+		sCriteria += "&" + "searchCriteria[filterGroups][0][filters][0][value]="+email
+
+		customers = self._getData('customers/search?'+ sCriteria)
+		for customer in customers["items"]:
+			return customer
+
+		return None
+
+	def getPoints(self, customerId):
+		res = self._getData('rewards/mine/balance?customer_id='+ str(customerId))
+
+		return res
+
+	def add(self, customerId, qty, comment="Modificados por el administrador", expire=365, action="admin"):
+		#https://amasty.com/knowledge-base/what-amasty-magento-2-plugins-support-api.html#reward
+		data = {
+			"customer_id": customerId, 
+			"amount": qty,
+			"comment": comment,
+			"action": action,
+			"expire": {"expire": True, "days": expire}
+		}
+		res = self._getData('rewards/management/points/add', data)
+		
+		return res
+
+	def deduct(self, customerId, qty, comment="Eliminados por el administrador", action="admin"):
+		data = {
+			"customer_id": customerId, 
+			"amount": qty,
+			"comment": comment,
+			"action": action
+		}
+		res = self._getData('rewards/management/points/deduct', data)
+		
+		return res
+		
+	def getRule(self, ruleId = 8):
+		return self._getData('rewards/management/rule?rule_id=' + str(ruleId))
+	# END: New methods (Amasty reward extension)
+
+
+
+
 
 	def getBalance(self, email):
 		res = self._getData('mwRewardpoints/getBalanceByEmail/'+email+'/0')
@@ -74,6 +163,12 @@ class Fichas():
 		_m = re.search('alencia', posName)
 		if _m:
 			groupId = 20
+		_m = re.search('arcelona', posName)
+		if _m:
+			groupId = 21
+		_m = re.search('itoria', posName)
+		if _m:
+			groupId = 22
 
 		customer = {
 			'customer': {
@@ -88,7 +183,10 @@ class Fichas():
 		}
 		res = self._postData('customers', customer)
 		#print(res)
-		self.setBalance(email, 75, 'Registered in Odoo POS');
+
+		#self.setBalance(email, 75, 'Registered in Odoo POS');
+		mCustomer = self.getCustomerByEmail(email)
+		self.add(self, mCustomer["id"], 75, "Registro en tienda.")
 
 		return res
 
@@ -96,7 +194,11 @@ class Fichas():
 		res = self._getData('mwRewardpoints/getProductRewardPoints/'+sku)
 		m = re.search('^Product SKU \(([^\)]*)\) has ([0-9]*) reward points', res)
 
-		points = int(m.group(2))
+		if m == None:
+			points = 0
+		else:
+			points = int(m.group(2))
+
 		return points
 
 	def findProductByBarcode(self, barcode):
@@ -118,12 +220,12 @@ class Fichas():
 
 
 
-client = Fichas('https://zacatrus.es/rest/V1/', 'odoo', 'p9U4Lap0vF')
+client = Fichas('https://zacatrus.es/rest/all/V1/', 'zaca24', 'carracacosa1')
 
 #res = client.createCustomer('sergio_fichas@infovit.net', 'Sergio', 'Madrid')
 #amount = client.getBalance('sergio_fichas@infovit.net')
 #client.setBalance('sergio@infovit.net', 100, 'pruebas')
-res = client.getBalance('sergio@infovit.net')
+res = client.getPoints(1)
 #res = client.getProductPoints('ZAC011')
 #res = client.searchCustomer('sergio@infovit.net')
 print( res )
