4c3b1552b583fc9b9cca8d2c020113113c86a0d4
felixVillafranca <f.villafranca@ingetive.com>
2021-05-04 05:51:10 +0200

    Eliminar pos-verifone-pad

--- pos_verifone_pinpad/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-# © 2018 FactorLibre - Hugo Santos <hugo.santos@factorlibre.com>
-from . import pos_config
-from . import account_journal
-from . import account_bank_statement_line
