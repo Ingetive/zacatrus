dac25a4f93d1eef939f15cbe3202ed754bfebecc
felixvillafranca <f.villafranca@ingetive.com>
2021-05-10 06:28:23 +0000

    Sustituir campo full_product_name por display_name en la vista de de pedidos del pos

--- zacatrus/__manifest__.py
+++ zacatrus/__manifest__.py
@@ -25,12 +25,14 @@
         'base',
         'product',
         'sale',
+        'point_of_sale'
     ],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
+        'views/point_of_sale.xml',
         #'views/templates.xml',
     ],
     # only loaded in demonstration mode
