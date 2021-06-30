fd407e37b74e13da521fcbf75ea92b303f3669c8
Sergio <sergio@infovit.net>
2021-06-30 13:01:05 +0200

    .

--- zacasocios/static/src/js/FichasSystem.js
+++ zacasocios/static/src/js/FichasSystem.js
@@ -340,5 +340,17 @@ odoo.define('zacasocios.TarjezacaSystem', function(require) {
     console.log("registering Tarjezaca controls...");
     Registries.Component.add(TarjezacaSystem);
 
+    var screens = require('point_of_sale.screens');
+    var PaymentScreenWidget = screens.PaymentScreenWidget;
+
+    PaymentScreenWidget.include({
+
+        validate_order: function(force_validation) {
+            console.log('We should validate Tarjezacas here.');
+            this._super(force_validation);
+        },
+
+    });
+
     return TarjezacaSystem;
 });
