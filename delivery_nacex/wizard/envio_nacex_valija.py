6a6afcb342e12341e633e83ed80ed9f60369e419
felixVillafranca <f.villafranca@ingetive.com>
2021-10-27 05:43:14 +0000

    Ajuste tratamiento envio valijas

--- delivery_nacex/wizard/envio_nacex_valija.py
+++ delivery_nacex/wizard/envio_nacex_valija.py
@@ -2,6 +2,7 @@
 # © 2021 Voodoo - <hola@voodoo.es>
 
 from odoo import api, fields, models
+from odoo.exceptions import UserError
 
 class EnvioValija(models.TransientModel):
     _name = 'delivery_nacex.envio_valija'
@@ -11,19 +12,20 @@ class EnvioValija(models.TransientModel):
 
     def action_envio_nacex_valija_apply(self):
         #TODO
-        #Los albaranes tienen que estar en estado ready
-        #Los albaranes tienen que tener metodo de transporte Nacex Valija
-        #Los albaranes no tienen que tener Padre
-        #Vista stock picking -> mostrar albaran contenedor solo si el metodo de envio es valija
-        #Actualizar documento instrucciones
-        nacex = self.env['delivery.carrier'].search([('delivery_type','=','nacex')])
+        #Los albaranes tienen que estar en estado ready???
+        #Los albaranes tienen que tener metodo de transporte Nacex Valija???
+        #Los albaranes no tienen que tener Padre???
+        #Vista stock picking -> mostrar albaran contenedor solo si el metodo de envio es valija ???
+        nacex = self.env.ref('delivery_nacex.delivery_carrier_nacex')
+        nacex_valija = self.env.ref('delivery_nacex.delivery_carrier_nacex_valija')
         picking_contenedor = False
         pickings = self.env['stock.picking'].browse(self.env.context.get('active_ids'))
         for albaran in pickings:
-            albaran.bultos = self.bultos
             if not picking_contenedor:
                 picking_contenedor = albaran
+                albaran.bultos = self.bultos
                 nacex.nacex_send_shipping(albaran)
-                albaran.carrier_id = nacex
+                albaran.carrier_id = nacex.id
             else:
                 albaran.picking_contenedor = picking_contenedor
+                albaran.carrier_id = nacex_valija
