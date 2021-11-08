5c4b76944fab54077c25a6b2bdbf2c8b643310d1
Adrian <adrian@voodoo.es>
2021-11-08 10:39:22 +0000

    cambios tarifas contactos

---
+++ zacatrus/models/ir_property.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class Property(models.Model):
+    _inherit = 'ir.property'
+    
+    @api.model
+    def create(self, vals):
+        res = super(Property, self).create(vals)
+        if res.name == 'property_product_pricelist':
+            res.sync_tarifa_companies()
+        return res
+    
+    def write(self, vals):
+        res = super(Property, self).write(vals)
+        self.sync_tarifa_companies()
+        return res
+    
+    def sync_tarifa_companies(self):
+        if self.env.context.get('no_update_tarifa'):
+            return
+        
+        for r in self:
+            if r.name == 'property_product_pricelist':
+                value_reference = r.value_reference
+                    
+                companies = self.env["res.company"].search([('id', '!=', r.company_id.id)])
+                for company in companies:
+                    property = self.env['ir.property'].sudo().search([
+                        ('name', '=', r.name),
+                        ('res_id', '=', r.res_id),
+                        ('fields_id', '=', r.fields_id.id),
+                        ('type', '=', 'many2one'),
+                        ('company_id', '=', company.id)
+                    ], limit=1)
+
+                    if property:
+                        if property.value_reference != value_reference:
+                            property.with_context(no_update_tarifa=True).write({'value_reference': value_reference})
+                    else:
+                        self.env['ir.property'].sudo().create({
+                            'name': r.name,
+                            'res_id': r.res_id,
+                            'company_id': company.id,
+                            'type': 'many2one',
+                            'fields_id': r.fields_id.id,
+                            'value_reference': value_reference
+                        }) 
