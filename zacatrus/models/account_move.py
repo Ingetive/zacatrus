6ad2c0e7907cdec56f7ed59657016c93ba9270b9
Adrian <adrian@voodoo.es>
2021-12-17 10:14:55 +0000

    metodo registrar pago factura

---
+++ zacatrus/models/account_move.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+import logging
+
+from odoo import models, fields, api
+
+_logger = logging.getLogger(__name__)
+
+
+class AccountMove(models.Model):
+    _inherit = 'account.move'
+    
+    @api.model
+    def registrar_pago(self, move_id, journal_id=None):
+        move = self.sudo().browse(move_id)
+        if not move:
+            return False
+        
+        payment_register = self.env['account.payment.register'].with_context({
+            'active_model': 'account.move',
+            'active_ids': [move.id],
+        }).create({})
+        
+        if journal_id:
+            payment_register.write({'journal_id': journal_id})
+            
+        payment_register.action_create_payments()
+        return True
+        
