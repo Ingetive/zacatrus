f1861ad3eaddd2bd19c4856cda65a9bb46c83a26
Adrian <adrian@voodoo.es>
2022-01-28 07:30:31 +0000

    fix envios nacex

--- zacatrus/models/.ipynb_checkpoints/__init__-checkpoint.py
+++ zacatrus/models/.ipynb_checkpoints/__init__-checkpoint.py
@@ -1,7 +1,16 @@
 # -*- coding: utf-8 -*-
 
+from . import account_move
+from . import account_move_line
 from . import product_template
 from . import sale_order
 from . import res_partner
 from . import res_users
+from . import stock_move
+from . import stock_valuation_layer
 from . import stock_picking
+from . import stock_inventory
+from . import purchase_order_line
+from . import pos_session
+from . import pos_payment
+from . import ir_property
