e57ce340477c5b173d9e4c90e0997740001f8e89
Adrián <a.delrio@ingetive.com>
2022-09-30 08:22:52 +0200

    actualizar gitignore

--- zacatrus/models/.ipynb_checkpoints/res_partner-checkpoint.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
-
-
-class Partner(models.Model):
-    _inherit = 'res.partner'
-
-    x_partner_id8 = fields.Char("Id. en Odoo 8")
--- zacatrus/models/.ipynb_checkpoints/res_users-checkpoint.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
-
-
-class Users(models.Model):
-    _inherit = 'res.users'
-
-    x_partner_id8 = fields.Char("Id. en Odoo 8")
