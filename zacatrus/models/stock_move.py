5862da2df484b2e49a776a89722f8d668b4895fa
Adrian <adrian@voodoo.es>
2022-10-24 06:51:55 +0000

    tracking pedido amazon

--- zacatrus/models/stock_move.py
+++ zacatrus/models/stock_move.py
@@ -1,9 +1,36 @@
 # -*- coding: utf-8 -*-
 
+import logging
+
 from odoo import models, fields, api
+
+_logger = logging.getLogger(__name__)
 
 
 class StockMove(models.Model):
     _inherit = 'stock.move'
 
     product_barcode = fields.Char(related='product_id.barcode', string = "Código de barras")
+    
+    @api.model
+    def create(self, vals):
+        _logger.warning("create")
+        _logger.warning(vals)
+        
+        res = super().create(vals)
+        
+        _logger.warning("despues de create")
+        _logger.warning(res.id)
+        _logger.warning(res.location_id)
+        
+        return res
+        
+        
+    
+    def write(self, vals):
+        _logger.warning("write")
+        _logger.warning(self.ids)
+        _logger.warning(vals)
+        res = super().write(vals)
+        
+        return res
