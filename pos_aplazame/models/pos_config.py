a4b1435cdbd0c8b9c19c8601ff538421b6dcd152
Sergio (fantasma) <sergio@infovit.net>
2022-11-16 21:40:31 +0100

    api key field

--- pos_aplazame/models/pos_config.py
+++ pos_aplazame/models/pos_config.py
@@ -6,6 +6,6 @@ from odoo import api, fields, models
 _logger = logging.getLogger(__name__)   
 
 class PosConfig(models.TransientModel):
-    _inherit = 'res.config'
+    _inherit = 'pos.config'
 
     x_aplazame_key = fields.Char("Aplazame private api key")
