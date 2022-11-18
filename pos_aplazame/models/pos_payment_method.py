4f91fd7962ece89f5193660a2669f4b34629f1d1
Sergio <sergio@infovit.net>
2022-11-18 15:57:33 +0100

    fix

--- pos_aplazame/models/pos_payment_method.py
+++ pos_aplazame/models/pos_payment_method.py
@@ -64,13 +64,7 @@ class PosPaymentMethod(models.Model):
             customer['phone'] = data['phone']
         dataParams = {  
             "merchant": {
-                "ipn_url": "https://mozo.zacatrus.es",
-                "notification_url": "https://mozo.zacatrus.es",
-                "success_url": "/",
-                "pending_url": "/",
-                "error_url": "/",
-                "dismiss_url": "/",
-                "ko_url": "/",
+                "ipn_url": "https://mozo.zacatrus.es"
             },
             "order": {
                 "id": orderId,
