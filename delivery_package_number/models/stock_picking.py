3007e5322675593608811a23d94c45e1e52445fc
Sergio <sergio@infovit.net>
2022-12-02 16:43:11 +0100

    Preguntar por el número de paquetes siempre cuando es DHL

--- delivery_package_number/models/stock_picking.py
+++ delivery_package_number/models/stock_picking.py
@@ -1,6 +1,7 @@
 # Copyright 2020 Tecnativa - David Vidal
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from odoo import api, fields, models
+from odoo.tools.float_utils import float_compare, float_is_zero, float_round
 
 
 class StockPicking(models.Model):
@@ -18,3 +19,19 @@ class StockPicking(models.Model):
     def _compute_number_of_packages(self):
         for picking in self:
             picking.number_of_packages = len(picking.package_ids) or 1
+
+    def _check_immediate(self):
+        immediate_pickings = self.browse()
+        #precision_digits = self.env['decimal.precision'].precision_get('Product Unit of Measure')
+        #for picking in self:
+        #    if all(float_is_zero(move_line.qty_done, precision_digits=precision_digits) for move_line in picking.move_line_ids.filtered(lambda m: m.state not in ('done', 'cancel'))):
+        #        immediate_pickings |= picking
+        #return immediate_pickings
+
+        for picking in self:
+            if picking['carrier_id'].id == 12:
+                immediate_pickings |= picking
+            else:
+                immediate_pickings = super(StockPicking, self)._check_immediate()
+
+        return immediate_pickings
