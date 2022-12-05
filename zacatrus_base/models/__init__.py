50b08b65eff21d8abc49256558bcc03ea2cc63cf
Sergio (fantasma) <sergio@infovit.net>
2022-12-05 21:08:21 +0100

    Asigna DHL para algunos pedidos de 'valija'

--- zacatrus_base/models/__init__.py
+++ zacatrus_base/models/__init__.py
@@ -1,3 +1,4 @@
 from . import res_config_settings
 from . import models
 from . import ir_attachment
+from . import stock_picking
---
+++ zacatrus_base/models/stock_picking.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+import logging
+
+from odoo import models, fields, api
+
+_logger = logging.getLogger(__name__)
+
+
+class Picking(models.Model):
+    _inherit = 'stock.picking'
+
+    def setPartnerCarrier(self):    
+        for picking in self:
+            if not picking.picking_type_id.id == 5 or not picking.partner_id or not picking.partner_id.property_delivery_carrier_id:
+                return False
+            if not picking.partner_id or not picking.partner_id.property_delivery_carrier_id:
+                return False
+
+            newCarrierId = picking.partner_id.property_delivery_carrier_id.id
+            # If valija
+            if newCarrierId == 9:
+                if picking.origin.endswith("-DHL"):
+                    newCarrierId = 12 # DHL B2B
+
+            picking.write({
+              'carrier_id' : newCarrierId
+            })
