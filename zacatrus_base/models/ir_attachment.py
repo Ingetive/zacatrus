f000d1fbd8259935e7860b301e6d0e4f3dedbdf3
Sergio <sergio@infovit.net>
2022-12-07 12:12:42 +0100

    QA

--- zacatrus_base/models/ir_attachment.py
+++ zacatrus_base/models/ir_attachment.py
@@ -8,25 +8,26 @@ from odoo import models, fields, api
 class Attachment(models.Model):
     _inherit = 'ir.attachment'    
     
-    def remotePrint(self, record):
-        if record.res_model == 'stock.picking':
-            printnodeKey = self.env['ir.config_parameter'].sudo().get_param('zacatrus_base.printnode_key')
-            printerId = self.env['ir.config_parameter'].sudo().get_param('zacatrus_base.dhl_segovia_printer_id')
-            if printnodeKey and printerId:
-                pickings = self.env['stock.picking'].sudo().search_read([
-                    ('id', '=', record.res_id)
-                ], ['carrier_id', 'picking_type_id'])
+    def remotePrint(self, record = None): 
+        for attachment in self:
+            if attachment.res_model == 'stock.picking':
+                printnodeKey = self.env['ir.config_parameter'].sudo().get_param('zacatrus_base.printnode_key')
+                printerId = self.env['ir.config_parameter'].sudo().get_param('zacatrus_base.dhl_segovia_printer_id')
+                if printnodeKey and printerId:
+                    pickings = self.env['stock.picking'].sudo().search_read([
+                        ('id', '=', attachment.res_id)
+                    ], ['carrier_id', 'picking_type_id'])
 
-                for picking in pickings:
-                    # Es DHL y sale de Segovia
-                    if picking['carrier_id'][0] == 12 and picking['picking_type_id'][0] == 5:
-                        data = {
-                          "content": record.datas ,
-                          "printerId": int(printerId),
-                          "contentType": "raw_base64",
-                          "title": record.res_name
-                        }
-                        requests.post(
-                            "https://api.printnode.com/printjobs", auth=(printnodeKey, ""),
-                            json=data
-                        )
+                    for picking in pickings:
+                        # Es DHL y sale de Segovia
+                        if picking['carrier_id'][0] == 12 and picking['picking_type_id'][0] == 5:
+                            data = {
+                              "content": attachment.datas ,
+                              "printerId": int(printerId),
+                              "contentType": "raw_base64",
+                              "title": attachment.res_name
+                            }
+                            requests.post(
+                                "https://api.printnode.com/printjobs", auth=(printnodeKey, ""),
+                                json=data
+                            )
