8ef1213c2c6ff180064671c4d060616eaeed29c1
Adrian <adrian@voodoo.es>
2022-12-19 14:00:43 +0000

    cambios conciliacion (incompleto)

--- conciliacion/models/account_bank_statement.py
+++ conciliacion/models/account_bank_statement.py
@@ -9,5 +9,8 @@ from odoo.exceptions import UserError, ValidationError
 class AccountBankStatement(models.Model):
     _inherit = "account.bank.statement"
     
+    nombre_fichero = fields.Char("Nombre fichero importado")
+    neto_importado = fields.Monetary("Neto importado")
+    
     def action_importar_transacciones(self):
         return self.env["conciliacion.importar_transacciones"].create({"bank_statement_id": self.id}).open_wizard()
--- conciliacion/wizard/conciliacion_importar_transacciones.py
+++ conciliacion/wizard/conciliacion_importar_transacciones.py
@@ -11,6 +11,8 @@ import io
 from datetime import datetime
 from odoo.exceptions import Warning
 from odoo import models, fields, exceptions, api, _
+from odoo.tools import DEFAULT_SERVER_DATE_FORMAT as DATE_FORMAT
+from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT as DATETIME_FORMAT
 
 _logger = logging.getLogger(__name__)
 
@@ -26,53 +28,91 @@ class ImportarTransacciones(models.Model):
         decrypted = base64.b64decode(self.fichero).decode('utf-8')
         with io.StringIO(decrypted) as fp:
             neto = 0
-            comisiones = 0
-            transferencias_banco = 0
+            comisiones_venta = 0
+            comisiones_transferencia = 0
+            transferencia = 0
+            fecha = None
             
             reader = csv.reader(fp, delimiter=",", quotechar='"')
             for line in reader:
-                if not line:
-                    pass
+                
+                if not line or len(line) < 8:
+                    continue
+                elif line[7] == "Fee":
+                    comisiones_transferencia = self._convert_to_float(line[14])
+                elif line[7] == "MerchantPayout":
+                    transferencia = self._convert_to_float(line[14])
+                elif line[7] in ["Settled", "Refunded"]:
+                    if not fecha:
+                        fecha = self._convert_to_date(line[5])
+                    debit = self._convert_to_float(line[10])
+                    credit = self._convert_to_float(line[11])
+                    neto += credit - debit
 
-                try:  debit = float(line[10])
-                except: debit = 0
-
-                try:  credit = float(line[11])
-                except: credit = 0
-
-                neto += credit - debit
-
-                try:  markup = float(line[17])
-                except: markup = 0
-
-                try:  scheme_fees = float(line[18])
-                except: scheme_fees = 0
-
-                comisiones -= markup + scheme_fees
-
-                try:  interchange = float(line[19])
-                except: interchange = 0
-
-                transferencias_banco -= interchange
+                    commission = self._convert_to_float(line[16])
+                    markup = self._convert_to_float(line[17])
+                    scheme_fees = self._convert_to_float(line[18])
+                    interchange = self._convert_to_float(line[19])
+                    comisiones_venta += interchange + markup + scheme_fees + commission
+                
+            sequence = 0
+            if fecha:
+                amls = self.env['account.move.line'].search([
+                    # ('journal_id.id', '=', self.env.ref("point_of_sale.pos_sale_journal").id),
+                    ('account_id', '=', self.env.ref("l10n_es.1_account_common_4300").id),
+                    ('ref', '=ilike', f"POS/{fecha.strftime('%Y/%m/%d')}/%"),
+                    ('name', 'ilike', "Adyen")
+                ])
+                
+                for aml in amls:
+                    payment_ref = f"{aml.move_id.name} : {aml.name} : {aml.ref}"
+                    self.env['account.bank.statement.line'].create({
+                        'sequence': sequence,
+                        'payment_ref': payment_ref,
+                        'statement_id': self.bank_statement_id.id,
+                        'amount': aml.debit or (aml.credit * -1)
+                    })
+                    sequence += 1
 
             self.env['account.bank.statement.line'].create({
-                'payment_ref': "Neto",
+                'payment_ref': "Comisiones venta",
                 'statement_id': self.bank_statement_id.id,
-                'amount': neto
+                'amount': comisiones_venta * -1
+            })
+            sequence += 1
+            
+            self.env['account.bank.statement.line'].create({
+                'sequence': sequence,
+                'payment_ref': "Comisiones transferencia",
+                'statement_id': self.bank_statement_id.id,
+                'amount': comisiones_transferencia * -1
+            })
+            sequence += 1
+            
+            self.env['account.bank.statement.line'].create({
+                'sequence': sequence,
+                'payment_ref': "Transferencia",
+                'statement_id': self.bank_statement_id.id,
+                'amount': transferencia * -1
             })
 
-            self.env['account.bank.statement.line'].create({
-                'payment_ref': "Comisiones",
-                'statement_id': self.bank_statement_id.id,
-                'amount': comisiones
+            self.bank_statement_id.write({
+                'neto_importado': neto
             })
-
-            self.env['account.bank.statement.line'].create({
-                'payment_ref': "Transferencias a bancos",
-                'statement_id': self.bank_statement_id.id,
-                'amount': transferencias_banco
-            })
+    
+    @api.model
+    def _convert_to_float(self, num_str):
+        try:  
+            return float(num_str)
+        except:
+            return 0.0
         
+    @api.model
+    def _convert_to_date(self, date_str):
+        try:  
+            return datetime.strptime(date_str, DATETIME_FORMAT).date()
+        except:
+            return None
         
     def open_wizard(self, context=None):
         return {
