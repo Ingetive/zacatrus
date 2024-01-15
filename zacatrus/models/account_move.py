22a7cdfd704cfb5c74182614b81a9e099edaafe2
Adrian <adrian@voodoo.es>
2024-01-15 10:18:11 +0000

    Cambiar el registro de pago

--- zacatrus/models/account_move.py
+++ zacatrus/models/account_move.py
@@ -11,14 +11,15 @@ class AccountMove(models.Model):
     _inherit = 'account.move'
     
     @api.model
-    def registrar_pago(self, move_id, journal_id=None):
-        move = self.sudo().browse(move_id)
-        if not move:
+    def registrar_pago(self, move_ids, amount, journal_id=None):
+        moves = self.sudo().browse(move_ids)
+        if not moves:
             return False
         
         payment_register = self.env['account.payment.register'].with_context({
             'active_model': 'account.move',
-            'active_ids': [move.id],
+            'amount': amount,
+            'active_ids': [(6, 0, moves.ids)],
         }).create({})
         
         if journal_id:
