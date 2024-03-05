30a5ca7fb4384a4b7e28f0ebfdcca7cb4dca1dcb
gaso <gustavo2840@gmail.com>
2024-03-04 21:28:17 -0500

    [MIG] nacex: estabilizacion a V16. #task=2838

--- delivery_nacex/models/stock_package_type.py
+++ delivery_nacex/models/stock_package_type.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-# © 2021 Voodoo - <hola@voodoo.es>
+# © 2024 Voodoo - <hola@voodoo.es>
 
 from odoo import fields, models
 
 
-class ProductPackaging(models.Model):
-    _inherit = 'product.packaging'
+class PackageType(models.Model):
+    _inherit = 'stock.package.type'
 
     package_carrier_type = fields.Selection(selection_add=[('nacex', 'Nacex')])
