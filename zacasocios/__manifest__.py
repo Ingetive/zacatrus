5316d0f0a5cc4fde44065f4c912c0bd053447a60
gaso <gustavo2840@gmail.com>
2024-03-06 20:02:13 -0500

    [MIG] zacasocios: migración general de modulo a estructura V16. #task=2838

--- zacasocios/__manifest__.py
+++ zacasocios/__manifest__.py
@@ -1,41 +1,24 @@
 # -*- coding: utf-8 -*-
 {
     'name': "zacasocios",
-
     'summary': """
         Permite gestionar Zacasocios """,
-
     'description': """
         Asigna y permite redimir Fichas
     """,
-
     'author': "Zacatrus",
     'website': "https://zacatrus.es",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
-    # for the full list
     'category': 'Uncategorized',
-    'version': '14.0',
-
-    # any module necessary for this one to work correctly
+    'version': '16.0',
     'depends': [
         "zacatrus_base", 
         'point_of_sale'
     ],
-
-    # always loaded
     'data': [
         'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
     ],
-    'qweb': [
-        'static/src/xml/Fichas.xml',
-    ],
+    # 'qweb': [
+    #     'static/src/xml/Fichas.xml', # TODO: Migración => Evaluar en OWL esta personalización
+    # ],
     'installable': True,
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
 }
