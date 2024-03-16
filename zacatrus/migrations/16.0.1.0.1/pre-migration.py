8ab0403bfe59c891921c0e8eb28d8f24cb10856c
gaso <gustavo2840@gmail.com>
2024-03-15 20:54:36 -0500

    [MIG] *: script de premigracion para mover ids de reportes en modulo de printnode y evitar error de dependencia

--- zacatrus/migrations/16.0.1.0.1/pre-migration.py
+++ zacatrus/migrations/16.0.1.0.1/pre-migration.py
@@ -4,6 +4,8 @@ from odoo import SUPERUSER_ID, api
 
 
 def migrate(cr, version):
+
+    # Eliminar vistas de modulos no migrados
     cr.execute(
         """
             delete from ir_ui_view where arch_fs ilike '%delivery_dhl_parcel%';
@@ -11,3 +13,26 @@ def migrate(cr, version):
             delete from ir_ui_view where arch_fs ilike '%stock_location_children%';
         """
     )
+
+    # Solucionar incidencia de error actualización printnode
+    # 1. Añadir el campo report_id_v14 si aún no existe
+    cr.execute("""
+        DO $$
+        BEGIN
+            IF NOT EXISTS (SELECT 1 FROM information_schema.columns 
+                           WHERE table_name='printnode_scenario' AND column_name='report_id_v14') THEN
+                ALTER TABLE printnode_scenario ADD COLUMN report_id_v14 integer;
+            END IF;
+        END
+        $$;
+    """)
+
+    # 2. Copiar los valores de report_id a report_id_v14
+    cr.execute("""
+        UPDATE printnode_scenario SET report_id_v14 = report_id;
+    """)
+
+    # 3. Actualizar el valor de report_id a 29 en todos los registros
+    cr.execute("""
+        UPDATE printnode_scenario SET report_id = 29;
+    """)
