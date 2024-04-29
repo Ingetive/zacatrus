0e5143faa5f8ce1a45fc244bd80c17412a302ff8
Sergio Viteri <sergio@infovit.net>
2024-04-29 10:05:38 +0200

    .

--- zacatrus_base/models/delivery_carrier.py
+++ zacatrus_base/models/delivery_carrier.py
@@ -11,11 +11,11 @@ class DeliveryCarrier(models.Model):
         _logger.debug(f"Zacalog: dhl_parcel_send_shipping")
 
         for picking in pickings:
-        if picking.carrier_id == 13 and picking.number_of_packages > 1:
-            picking.write({
-                'carrier_id' : newCarrierId
-            })
-            _logger.debug(f"Zacalog: dhl_parcel_send_shipping modificado {picking['name']}")
+            if picking.carrier_id == 13 and picking.number_of_packages > 1:
+                picking.write({
+                    'carrier_id' : newCarrierId
+                })
+                _logger.debug(f"Zacalog: dhl_parcel_send_shipping modificado {picking['name']}")
 
 
         super().dhl_parcel_send_shipping(pickings)
