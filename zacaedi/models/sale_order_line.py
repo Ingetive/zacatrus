8228e3dff544b7e588eafc2b97994b248b6d00d0
Sergio Viteri <sergio@infovit.net>
2024-08-28 18:56:06 +0200

    Bloquear pedidos EDI

--- zacaedi/models/sale_order_line.py
+++ zacaedi/models/sale_order_line.py
@@ -2,6 +2,7 @@
 import logging
 
 from odoo import models, fields, api
+from odoo.exceptions import UserError
 
 _logger = logging.getLogger(__name__)
 
@@ -10,3 +11,10 @@ class SaleOrderLine(models.Model):
 
     x_edi_line = fields.Char(readonly=False)
     x_edi_product = fields.Char(readonly=False)
+
+    @api.model
+    def create(self, vals):
+        order = self.env['sale.order'].browse(vals['order_id'])
+        if order.x_edi_order:
+            raise UserError('No se pueden añadir líneas a un pedido que viene por EDI.')
+        return super(SaleOrderLine, self).create(vals)
